143df205d2ea9e40d0f4115deb18d65706febb6f
Isma Navarro <ismanapa@gmail.com>
2022-06-13 09:37:42 +0200

    feat(narrowing): extract email regex to variable

--- src/narrowing/catch-narrowing/Email.ts
+++ src/narrowing/catch-narrowing/Email.ts
@@ -2,10 +2,14 @@ import { InvalidEmailError } from "./InvalidEmailError";
 import { StringValueObject } from "./StringValueObject";
 
 export class Email extends StringValueObject {
+  private readonly nonRepresentativeEmailRegEx =
+    /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
   constructor(readonly value: string) {
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
+    super(value);
+
+    if (!this.nonRepresentativeEmailRegEx.test(value)) {
       throw new InvalidEmailError(value);
     }
-    super(value);
   }
 }
