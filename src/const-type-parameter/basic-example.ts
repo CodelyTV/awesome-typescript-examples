80f3c6c6f3c88591ce9d6a795231dace97385839
Isma Navarro <ismanapa@gmail.com>
2023-07-26 18:05:08 +0200

    feat: const type parameter basic example

---
+++ src/const-type-parameter/basic-example.ts
@@ -0,0 +1,29 @@
+// Example with "as const"
+export type User = { name: string, age: number };
+
+function getUserName<T extends User>(username: T): T["name"] {
+  return username.name;
+}
+
+const userName = getUserName({ name: "Isma", age: 31 } as const);
+//    ^?
+
+// Ejemplo const type parameter
+function getUserAge<const T extends User>(username: T): T["age"] {
+  return username.age;
+}
+
+const userAge = getUserAge({ name: "Isma", age: 31 });
+//    ^?
+
+
+// Ejemplo con array y readonly
+// En este ejemplo es importante combinar el const con el readonly  porque si no el array sería
+// mutable dentro de la función y no se infiere el tipo
+type HasNames = { names: readonly string[] };
+function getNamesExactly<const T extends HasNames>(arg: T): T["names"] {
+  return arg.names;
+}
+
+const names = getNamesExactly({ names: ["Javi", "Ronny", "Isma"] });
+//     ^?
